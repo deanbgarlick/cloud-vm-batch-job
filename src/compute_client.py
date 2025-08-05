6a5aa1da6ccd53fe3c075bf7c51216fec0710e90
Daniel Garlick <deanbgarlick@gmail.com>
2025-08-05 14:53:44 -0700

    Fix scope bug

--- src/compute_client.py
+++ src/compute_client.py
@@ -4,6 +4,7 @@ compute_client = None
 
 
 def get_compute_client():
+    global compute_client
     if compute_client is None:
         compute_client = compute_v1.InstancesClient()
     return compute_client
